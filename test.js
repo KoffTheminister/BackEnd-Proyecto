3f62734e633a156fde7f6964cc4162baf6c2aa9c
gonzalo carrizo <gonzacarrizo123456@gmail.com>
2024-05-22 16:03:04 -0300

    se creo el archivo principal del crud 1 y se probo el ingreso por teclado

---
+++ test.js
@@ -0,0 +1,30 @@
+// Ejercicio 551: Capturar datos del usuario usando la entrada estándar (teclado) usando Node.js.
+
+//const readline = require('readline');
+//let x
+//let interfazCaptura = readline.createInterface({
+    //input: process.stdin,
+    //output: process.stdout
+//});
+
+//interfazCaptura.question('¿Cuál es su correo electrónico?: ', function(respuesta){
+    //console.log(`Su correo electrónico es: ${respuesta}`);
+    //x =  respuesta
+    //interfazCaptura.close();
+//});
+
+//console.log(`Su correo electrónico es: ${x}`);
+var nombre;
+nombre = function prompt(data){
+
+};
+console.log(nombre.toString());
+
+//console.log("ingrse su nombre");
+
+//process.stdin.on('data', function(data){
+//    nombre = data.toString();
+//    console.log(nombre)
+//    process.exit();
+//})
+//console.log(nombre);
