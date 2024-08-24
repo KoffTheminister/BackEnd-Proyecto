83d4f57345548117ef9f46f90f77aff219c53c94
Jon Dafoe <juanzafran121@gmail.com>
2024-08-23 23:03:06 -0300

    primera coneccion con sql workbench

--- dist/guardiaDir/guardia.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { sanitizeGuardiaInput, getAll, getOne, add, update, deleteOne } from "./guardia.controller.js";
-export const guardiaRouter = Router();
-guardiaRouter.get('/', getAll);
-guardiaRouter.get('/:legajo', getOne);
-guardiaRouter.post('/', sanitizeGuardiaInput, add);
-guardiaRouter.put(':legajo', sanitizeGuardiaInput, update);
-guardiaRouter.delete('/:legajo', sanitizeGuardiaInput, deleteOne);
-//# sourceMappingURL=guardia.routes.js.map
